d2fe8f236ca5414fe35467c0d859e8b3816a9a92
Daniel Tan <danieltan380@gmail.com>
2023-08-10 17:23:01 -0700

    Added notes for how this function works

--- trashToTreasure.js
+++ trashToTreasure.js
@@ -1,9 +1,11 @@
 const smartGarbage = function(trash, bins){
   let keys = Object.keys(bins);
+  //"keys" is created to house all of the keys for bins.
   for (let x of keys){
     if (x === trash){
       bins[x]+= 1;
     }
+  //for loop is created to loop through all of bins' keys to find one (x) that matches "trash". bins[x] is used because bracket notation is needed, since "trash" contains a string. Finally, += 1 is used to increment the existing value of that key.
   }
   return(bins);
 };
